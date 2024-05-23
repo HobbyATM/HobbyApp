04c7f1481008c78d30ca8ad36142a2a78cd62600
Mert Akgün <mert_akgun2002@hotmail.com>
2024-05-24 01:04:45 +0300

    Sign_up phone number & fullname

--- app/src/main/java/com/example/hobbyapp/SignUpActivity.kt
+++ app/src/main/java/com/example/hobbyapp/SignUpActivity.kt
@@ -1,13 +1,9 @@
 package com.example.hobbyapp
 
-
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.hobbyapp.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
@@ -27,25 +23,24 @@ class SignUpActivity : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
 
-        binding.signupbutton.setOnClickListener(){
+        binding.signupbutton.setOnClickListener {
+            val username = binding.usernametext.text.toString()
+            val phone = binding.phonetext.text.toString()
             val email = binding.emailtext.text.toString()
             val password = binding.passwordtext.text.toString()
             val confirmPass = binding.passwordrepaettext.text.toString()
 
             val userMap = hashMapOf(
-                "name" to "username",
+                "username" to username,
+                "phone" to phone,
                 "email" to email,
                 "password" to password
             )
 
-
-            if (email.isNotEmpty() && password.isNotEmpty() && confirmPass.isNotEmpty()){
-
-                if (password == confirmPass)
-                {
-                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
-                        if (it.isSuccessful)
-                        {
+            if (email.isNotEmpty() && password.isNotEmpty() && confirmPass.isNotEmpty() && username.isNotEmpty() && phone.isNotEmpty()) {
+                if (password == confirmPass) {
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                        if (it.isSuccessful) {
                             val userId = FirebaseAuth.getInstance().currentUser!!.uid
 
                             db.collection("User").document(userId).set(userMap)
@@ -53,27 +48,20 @@ class SignUpActivity : AppCompatActivity() {
                                     val intent = Intent(this, SigninActivity::class.java)
                                     startActivity(intent)
                                 }
-                                .addOnFailureListener{
+                                .addOnFailureListener {
                                     Toast.makeText(this, "Failed!!", Toast.LENGTH_SHORT).show()
                                 }
 
-                        }
-                        else
-                        {
+                        } else {
                             Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
                         }
                     }
-                }
-                else
-                {
+                } else {
                     Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
                 }
-            }
-            else
-            {
-                Toast.makeText(this, "Empty Fields are now allowed !!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Empty Fields are not allowed!!", Toast.LENGTH_SHORT).show()
             }
         }
-
     }
 }

72aadd05617b7a537a9a94e950cfde158f0c91fa
Mert Akgün <mert_akgun2002@hotmail.com>
2024-05-28 16:44:53 +0300

    Settings SignOut Button Dialog

--- app/src/main/java/com/example/hobbyapp/Settings.kt
+++ app/src/main/java/com/example/hobbyapp/Settings.kt
@@ -1,6 +1,7 @@
 package com.example.hobbyapp
 
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
@@ -8,21 +9,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Settings.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Settings : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
     private lateinit var firebaseAuth: FirebaseAuth
@@ -47,10 +41,25 @@ class Settings : Fragment() {
 
         val signOutButton: Button = view.findViewById(R.id.signout_button)
         signOutButton.setOnClickListener {
-            signOut()
+            showSignOutDialog()
         }
 
         return view
+    }
+
+    private fun showSignOutDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Çıkış Yap")
+        builder.setMessage("Çıkış yapmak istediğinize emin misiniz?")
+        builder.setPositiveButton("Çıkış Yap") { dialog, _ ->
+            signOut()
+            dialog.dismiss()
+        }
+        builder.setNegativeButton("Vazgeç") { dialog, _ ->
+            dialog.dismiss()
+        }
+        val dialog = builder.create()
+        dialog.show()
     }
 
     private fun signOut() {
@@ -64,15 +73,6 @@ class Settings : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Settings.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             Settings().apply {
